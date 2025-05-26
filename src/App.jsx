c2d330a96bc98d673f2d6123ce0173d7bfb32bf2
Rodrigo Otreras <rodriotreras@gmail.com>
2025-05-26 17:58:23 -0300

    Inicio Proyecto

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-blue-600 text-white p-8 text-center">
+      <h1 className="text-3xl font-bold underline text-blue-500">
+        ¡Tailwind está funcionandoasdasdasd!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
